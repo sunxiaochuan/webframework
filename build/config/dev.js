b1a9aa0b0b685e6198cc5210e38dc5cb316eaf65
sunxiaochuan <1264086566@qq.com>
2018-03-22 19:32:28 +0800

    可运行

--- build/config/dev.js
+++ build/config/dev.js
@@ -1,11 +1,19 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 const dev = {
-    init:function(){
-        {
+    init: function () {
+        if (process.env.NODE_ENV == 'development') {
+            console.log('development');
+        }
+        if (process.env.NODE_ENV == 'tesing') {
+            console.log('tesing');
+        }
+        if (process.env.NODE_ENV == 'procuction') {
             console.log('procuction');
         }
     }
 };
-
-module.exports = dev;
+exports.default = dev;
